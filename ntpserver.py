1a314f886e55cdf5c080af6db128366e19447076
Gregory Kuhn <gregory.kuhn@bd.com>
2017-10-27 15:21:56 +0100

    Adding logging and broadcast capability

--- ntpserver.py
+++ ntpserver.py
@@ -5,13 +5,53 @@ import time
 import threading
 import select
 import sys
-if sys.version_info[0] == 2:
-    import Queue as queue
-else:
-    import queue
+import logging
+import logging.handlers
+import argparse
+import netifaces
+import queue
 
+logger = logging.getLogger()
 taskQueue = queue.Queue()
+packetQueue = queue.Queue()
 stopFlag = False
+
+def get_network_addresses():
+    ifaces = netifaces.interfaces()
+    bcast_addresses = []
+    interface_addresses = []
+    for iface in ifaces:
+        details = netifaces.ifaddresses(iface)
+        for k, vals in details.items():
+            if k == netifaces.AF_INET:
+                for addr in vals:
+                    bcast_addresses.append(addr['broadcast'])
+                    interface_addresses.append(addr['addr'])
+    return interface_addresses, bcast_addresses
+
+
+
+def setup_logger(logger, level=logging.INFO, file_path=None):
+    logger.setLevel(level)
+    console_formatter = logging.Formatter(
+            fmt='%(asctime)s - %(levelname)s - %(message)s')
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    if file_path:
+        #also add a file handler
+        file_formatter = logging.Formatter(
+            fmt='%(asctime)s - %(levelname)s - %(funcName)s - %(lineno)d - %(message)s')
+        file_handler = logging.handlers.TimedRotatingFileHandler(
+                filename=file_path,
+                when='D',
+                backupCount=7 #allow for a week of logs
+                )
+        file_handler.setFormatter(file_formatter)
+        logger.addHandler(file_handler)
+
+
 
 def system_to_ntp_time(timestamp):
     """Convert a system time to a NTP time.
@@ -126,7 +166,7 @@ class NTPPacket:
     _PACKET_FORMAT = "!B B B b 11I"
     """packet format to pack/unpack"""
 
-    def __init__(self, version=2, mode=3, tx_timestamp=0):
+    def __init__(self, version=4, mode=3, tx_timestamp=0):
         """Constructor.
 
         Parameters:
@@ -140,11 +180,11 @@ class NTPPacket:
         """version"""
         self.mode = mode
         """mode"""
-        self.stratum = 0
+        self.stratum = 1
         """stratum"""
-        self.poll = 0
+        self.poll = 10
         """poll interval"""
-        self.precision = 0
+        self.precision = -10
         """precision"""
         self.root_delay = 0
         """root delay"""
@@ -164,7 +204,56 @@ class NTPPacket:
         self.tx_timestamp_high = 0
         self.tx_timestamp_low = 0
         """tansmit timestamp"""
-        
+
+    def __str__(self):
+        '''create a nice string representation of the packet data modelled off
+        of format described in RFC2030:
+
+                     1                   2                   3
+       0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |LI | VN  |Mode |    Stratum    |     Poll      |   Precision   |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |                          Root Delay                           |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |                       Root Dispersion                         |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |                     Reference Identifier                      |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |                                                               |
+      |                   Reference Timestamp (64)                    |
+      |                                                               |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |                                                               |
+      |                   Originate Timestamp (64)                    |
+      |                                                               |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |                                                               |
+      |                    Receive Timestamp (64)                     |
+      |                                                               |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |                                                               |
+      |                    Transmit Timestamp (64)                    |
+      |                                                               |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |                 Key Identifier (optional) (32)                |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      |                                                               |
+      |                                                               |
+      |                 Message Digest (optional) (128)               |
+      |                                                               |
+      |                                                               |
+      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+'''
+        packet_str = f'''\
+LI|VN|Mode|Stratum|Poll|Precision: {self.leap}|{self.version}|{self.mode}|{self.stratum}|{self.poll}|{self.precision}
+Root Delay                       : {self.root_delay}
+Root Dispersion                  : {self.root_dispersion}
+Reference Identifier             : {self.ref_id}
+Reference Timestamp (64)         : {self.ref_timestamp}
+Originate Timestamp (64)         : {self.orig_timestamp}
+Receive Timestamp (64)           : {self.recv_timestamp}
+Transmit Timestamp (64)          : {self.tx_timestamp}'''
+        return packet_str
     def to_data(self):
         """Convert this NTPPacket to a buffer that can be sent over a socket.
 
@@ -237,82 +326,115 @@ class NTPPacket:
     def SetOriginTimeStamp(self,high,low):
         self.orig_timestamp_high = high
         self.orig_timestamp_low = low
-        
 
-class RecvThread(threading.Thread):
-    def __init__(self,socket):
+class workerThread(threading.Thread):
+    def __init__(self,socket, broadcast_interval, port):
         threading.Thread.__init__(self)
         self.socket = socket
+        self.broadcast_interval = broadcast_interval
+        logging.debug("Broadcast interval set to: %d", broadcast_interval)
+        self.port = port
+
+        if broadcast_interval:
+            self.interface_addresses, self.broadcast_addresses = get_network_addresses()
+            logger.debug("broadcast addresses: %s", self.broadcast_addresses)
+
     def run(self):
         global taskQueue,stopFlag
+        last_broadcast_time = time.time()
         while True:
             if stopFlag == True:
-                print("RecvThread Ended")
+                logger.info("workerThread Ended")
                 break
-            rlist,wlist,elist = select.select([self.socket],[],[],1);
+            rlist,wlist,elist = select.select([self.socket],[self.socket],[],1);
             if len(rlist) != 0:
-                print("Received %d packets" % len(rlist))
                 for tempSocket in rlist:
                     try:
                         data,addr = tempSocket.recvfrom(1024)
-                        recvTimestamp = recvTimestamp = system_to_ntp_time(time.time())
-                        taskQueue.put((data,addr,recvTimestamp))
+                        logger.info("Received packet from %s", addr[0])
+                        if addr[0] in self.interface_addresses:
+                            logger.debug("Ignoring broadcast from self")
+                            continue
                     except socket.error as msg:
-                        print(msg);
+                        logging.critical(msg);
+                    recvTimestamp = recvTimestamp = system_to_ntp_time(time.time())
+                    self.send_response(data,addr,recvTimestamp)
+            if len(wlist) != 0:
+                current_time = time.time()
+                if self.broadcast_interval and current_time - last_broadcast_time > self.broadcast_interval:
+                    last_broadcast_time = current_time
+                    for tempSocket in wlist:
+                        self.send_broadcast(tempSocket)
+            time.sleep(1)
 
-class WorkThread(threading.Thread):
-    def __init__(self,socket):
-        threading.Thread.__init__(self)
-        self.socket = socket
-    def run(self):
-        global taskQueue,stopFlag
-        while True:
-            if stopFlag == True:
-                print("WorkThread Ended")
-                break
-            try:
-                data,addr,recvTimestamp = taskQueue.get(timeout=1)
-                recvPacket = NTPPacket()
-                recvPacket.from_data(data)
-                timeStamp_high,timeStamp_low = recvPacket.GetTxTimeStamp()
-                sendPacket = NTPPacket(version=3,mode=4)
-                sendPacket.stratum = 2
-                sendPacket.poll = 10
-                '''
-                sendPacket.precision = 0xfa
-                sendPacket.root_delay = 0x0bfa
-                sendPacket.root_dispersion = 0x0aa7
-                sendPacket.ref_id = 0x808a8c2c
-                '''
-                sendPacket.ref_timestamp = recvTimestamp-5
-                sendPacket.SetOriginTimeStamp(timeStamp_high,timeStamp_low)
-                sendPacket.recv_timestamp = recvTimestamp
-                sendPacket.tx_timestamp = system_to_ntp_time(time.time())
-                socket.sendto(sendPacket.to_data(),addr)
-                print("Sended to %s:%d" % (addr[0],addr[1]))
-            except queue.Empty:
-                continue
-                
-        
-listenIp = "0.0.0.0"
-listenPort = 123
-socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-socket.bind((listenIp,listenPort))
-print("local socket: ", socket.getsockname());
-recvThread = RecvThread(socket)
-recvThread.start()
-workThread = WorkThread(socket)
-workThread.start()
+    def send_broadcast(self, socket):
+        broadcastPacket = NTPPacket()
+        broadcastPacket = NTPPacket(tx_timestamp = system_to_ntp_time(time.time()))
+        broadcastPacket.poll = 10
+        broadcastPacket.mode = 5
+        broadcastPacket.ref_timestamp = broadcastPacket.tx_timestamp-5
+        logger.debug("Broadcast Packet details: \n%s", broadcastPacket)
+        for bcast_address in self.broadcast_addresses:
+            self.socket.sendto(broadcastPacket.to_data(),(bcast_address, self.port))
+            logger.info("Sending broadcast packet to %s:%d", bcast_address,self.port)
 
-while True:
-    try:
-        time.sleep(0.5)
-    except KeyboardInterrupt:
-        print("Exiting...")
-        stopFlag = True
-        recvThread.join()
-        workThread.join()
-        #socket.close()
-        print("Exited")
-        break
-        
+
+    def send_response(self, data, addr, recvTimestamp):
+        recvPacket = NTPPacket()
+        recvPacket.from_data(data)
+        logger.debug("Received Packet details: \n%s", recvPacket)
+        timeStamp_high,timeStamp_low = recvPacket.GetTxTimeStamp()
+        sendPacket = NTPPacket(version=3,mode=4)
+        '''
+        sendPacket.precision = 0xfa
+        sendPacket.ref_id = 0x808a8c2c
+        '''
+        sendPacket.ref_timestamp = recvTimestamp-5
+        sendPacket.recv_timestamp = recvTimestamp
+        sendPacket.tx_timestamp = system_to_ntp_time(time.time())
+        self.socket.sendto(sendPacket.to_data(),addr)
+        logger.info("Sending response packet to %s:%d", addr[0],addr[1])
+        logger.debug("Sent Packet details: \n%s", sendPacket)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='SNTP server')
+    parser.add_argument('-p', '--port', type=int, default=123,
+        help='The port to listen on, defaults to 123')
+    parser.add_argument('-a', '--address', default='0.0.0.0',
+        help='The address to listen on, defaults to 0.0.0.0 (all interfaces)')
+    parser.add_argument('-v', action='store_true', help='use verbose logging')
+    parser.add_argument('-l', nargs=1, metavar='log_file_path',
+            help='additionally log to a file')
+    parser.add_argument('-b', type=int, default=0,
+            help='broadcast_interval in secs, defaults to 0 (no broadcast)')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+
+    p = get_parser()
+
+    log_file = None
+    if p.l:
+        log_file = p.l[0]
+    if p.v:
+        setup_logger(logger, level=logging.DEBUG, file_path=log_file)
+    else:
+        setup_logger(logger, level=logging.INFO, file_path=log_file)
+
+    socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    socket.bind((p.address,p.port))
+    logger.info("local socket: %s", socket.getsockname());
+    worker_thread = workerThread(socket, p.b, p.port)
+    worker_thread.start()
+
+    while True:
+        try:
+            time.sleep(0.5)
+        except KeyboardInterrupt:
+            logger.info("Exiting...")
+            stopFlag = True
+            worker_thread.join()
+            logger.info("Exited")
+            break
+

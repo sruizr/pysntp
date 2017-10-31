dbcdc960f235376d09230804be6c3d1306a53790
Gregory Kuhn <gregory.kuhn@bd.com>
2017-10-31 10:35:09 +0000

    Adding error injection capability

--- ntpserver.py
+++ ntpserver.py
@@ -9,12 +9,54 @@ import logging
 import logging.handlers
 import argparse
 import netifaces
-import queue
+import random
 
 logger = logging.getLogger()
-taskQueue = queue.Queue()
-packetQueue = queue.Queue()
 stopFlag = False
+
+class TimeToHighLow:
+    """Use descriptor rather than having to repeat a bunch of properties"""
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return self._to_time(
+                getattr(instance, self.name+'_high'),
+                getattr(instance, self.name+'_low')) - NTP.NTP_DELTA
+
+    def __set__(self, instance, value):
+        ntp_value = value + NTP.NTP_DELTA
+        high, low = self._to_high_low(ntp_value)
+        setattr(instance, self.name+'_high', high)
+        setattr(instance, self.name+'_low', low)
+
+
+    def _to_time(self, integ, frac, n=32):
+        """Return a timestamp from an integral and fractional part.
+
+        Parameters:
+        integ -- integral part
+        frac  -- fractional part
+        n     -- number of bits of the fractional part
+
+        Returns:
+        timestamp
+        """
+        return integ + float(frac)/2**n
+
+    def _to_high_low(self, timestamp):
+        """Return the high and low components of a timestamp
+
+        Parameters:
+            timestamp -- full floating point timestamp
+
+        Returns:
+            high,low
+        """
+        high = int(timestamp)
+        return high, int((timestamp-high)*(2**32))
+
+
 
 def get_network_addresses():
     ifaces = netifaces.interfaces()
@@ -87,20 +129,6 @@ def _to_frac(timestamp, n=32):
     """
     return int(abs(timestamp - _to_int(timestamp)) * 2**n)
 
-def _to_time(integ, frac, n=32):
-    """Return a timestamp from an integral and fractional part.
-
-    Parameters:
-    integ -- integral part
-    frac  -- fractional part
-    n     -- number of bits of the fractional part
-
-    Retuns:
-    timestamp
-    """
-    return integ + float(frac)/2**n	
-		
-
 
 class NTPException(Exception):
     """Exception raised by this module."""
@@ -162,9 +190,15 @@ class NTPPacket:
 
     This represents an NTP packet.
     """
-    
     _PACKET_FORMAT = "!B B B b 11I"
     """packet format to pack/unpack"""
+
+    #setup some properties by using common descriptor
+    orig_timestamp = TimeToHighLow()
+    recv_timestamp = TimeToHighLow()
+    tx_timestamp = TimeToHighLow()
+    ref_timestamp = TimeToHighLow()
+
 
     def __init__(self, version=4, mode=3, tx_timestamp=0):
         """Constructor.
@@ -174,36 +208,23 @@ class NTPPacket:
         mode         -- packet mode (client, server)
         tx_timestamp -- packet transmit timestamp
         """
-        self.leap = 0
+
         """leap second indicator"""
+        self.leap = 0
         self.version = version
-        """version"""
         self.mode = mode
-        """mode"""
         self.stratum = 1
-        """stratum"""
         self.poll = 10
-        """poll interval"""
         self.precision = -10
-        """precision"""
         self.root_delay = 0
-        """root delay"""
         self.root_dispersion = 0
-        """root dispersion"""
         self.ref_id = 0
-        """reference clock identifier"""
-        self.ref_timestamp = 0
-        """reference timestamp"""
+
+        #timestamps
+        self.ref_timestamp  = 0
         self.orig_timestamp = 0
-        self.orig_timestamp_high = 0
-        self.orig_timestamp_low = 0
-        """originate timestamp"""
         self.recv_timestamp = 0
-        """receive timestamp"""
         self.tx_timestamp = tx_timestamp
-        self.tx_timestamp_high = 0
-        self.tx_timestamp_low = 0
-        """tansmit timestamp"""
 
     def __str__(self):
         '''create a nice string representation of the packet data modelled off
@@ -254,6 +275,7 @@ Originate Timestamp (64)         : {self.orig_timestamp}
 Receive Timestamp (64)           : {self.recv_timestamp}
 Transmit Timestamp (64)          : {self.tx_timestamp}'''
         return packet_str
+
     def to_data(self):
         """Convert this NTPPacket to a buffer that can be sent over a socket.
 
@@ -273,20 +295,20 @@ Transmit Timestamp (64)          : {self.tx_timestamp}'''
                 _to_int(self.root_dispersion) << 16 |
                 _to_frac(self.root_dispersion, 16),
                 self.ref_id,
-                _to_int(self.ref_timestamp),
-                _to_frac(self.ref_timestamp),
-                #Change by lichen, avoid loss of precision
+                self.ref_timestamp_high,
+                self.ref_timestamp_low,
                 self.orig_timestamp_high,
                 self.orig_timestamp_low,
-                _to_int(self.recv_timestamp),
-                _to_frac(self.recv_timestamp),
-                _to_int(self.tx_timestamp),
-                _to_frac(self.tx_timestamp))
+                self.recv_timestamp_high,
+                self.recv_timestamp_low,
+                self.tx_timestamp_high,
+                self.tx_timestamp_low)
         except struct.error:
             raise NTPException("Invalid NTP packet fields.")
         return packed
 
-    def from_data(self, data):
+    @classmethod
+    def from_data(cls, data):
         """Populate this instance from a NTP packet payload received from
         the network.
 
@@ -302,32 +324,26 @@ Transmit Timestamp (64)          : {self.tx_timestamp}'''
         except struct.error:
             raise NTPException("Invalid NTP packet.")
 
-        self.leap = unpacked[0] >> 6 & 0x3
-        self.version = unpacked[0] >> 3 & 0x7
-        self.mode = unpacked[0] & 0x7
-        self.stratum = unpacked[1]
-        self.poll = unpacked[2]
-        self.precision = unpacked[3]
-        self.root_delay = float(unpacked[4])/2**16
-        self.root_dispersion = float(unpacked[5])/2**16
-        self.ref_id = unpacked[6]
-        self.ref_timestamp = _to_time(unpacked[7], unpacked[8])
-        self.orig_timestamp = _to_time(unpacked[9], unpacked[10])
-        self.orig_timestamp_high = unpacked[9]
-        self.orig_timestamp_low = unpacked[10]
-        self.recv_timestamp = _to_time(unpacked[11], unpacked[12])
-        self.tx_timestamp = _to_time(unpacked[13], unpacked[14])
-        self.tx_timestamp_high = unpacked[13]
-        self.tx_timestamp_low = unpacked[14]
+        instance = cls()
+        instance.leap = unpacked[0] >> 6 & 0x3
+        instance.version = unpacked[0] >> 3 & 0x7
+        instance.mode = unpacked[0] & 0x7
+        instance.stratum = unpacked[1]
+        instance.poll = unpacked[2]
+        instance.precision = unpacked[3]
+        instance.root_delay = float(unpacked[4])/2**16
+        instance.root_dispersion = float(unpacked[5])/2**16
+        instance.ref_id = unpacked[6]
+        instance.ref_timestamp_high  = unpacked[7]
+        instance.ref_timestamp_low   = unpacked[8]
+        instance.orig_timestamp_high = unpacked[9]
+        instance.orig_timestamp_low  = unpacked[10]
+        instance.recv_timestamp_high = unpacked[11]
+        instance.recv_timestamp_low  = unpacked[12]
+        instance.tx_timestamp_high   = unpacked[13]
+        instance.tx_timestamp_low    = unpacked[14]
 
-    def GetTxTimeStamp(self):
-        return (self.tx_timestamp_high,self.tx_timestamp_low)
-
-    def SetOriginTimeStamp(self,high,low):
-        self.orig_timestamp_high = high
-        self.orig_timestamp_low = low
-
-class workerThread(threading.Thread):
+class WorkerThread(threading.Thread):
     def __init__(self,socket, broadcast_interval, port):
         threading.Thread.__init__(self)
         self.socket = socket
@@ -339,12 +355,16 @@ class workerThread(threading.Thread):
             self.interface_addresses, self.broadcast_addresses = get_network_addresses()
             logger.debug("broadcast addresses: %s", self.broadcast_addresses)
 
+    def pre_send_hook(self, pkt):
+        "override me"
+        pass
+
     def run(self):
-        global taskQueue,stopFlag
+        global stopFlag
         last_broadcast_time = time.time()
         while True:
             if stopFlag == True:
-                logger.info("workerThread Ended")
+                logger.info("WorkerThread Ended")
                 break
             rlist,wlist,elist = select.select([self.socket],[self.socket],[],1);
             if len(rlist) != 0:
@@ -357,7 +377,7 @@ class workerThread(threading.Thread):
                             continue
                     except socket.error as msg:
                         logging.critical(msg);
-                    recvTimestamp = recvTimestamp = system_to_ntp_time(time.time())
+                    recvTimestamp = time.time()
                     self.send_response(data,addr,recvTimestamp)
             if len(wlist) != 0:
                 current_time = time.time()
@@ -365,37 +385,71 @@ class workerThread(threading.Thread):
                     last_broadcast_time = current_time
                     for tempSocket in wlist:
                         self.send_broadcast(tempSocket)
-            time.sleep(1)
+            time.sleep(0.1)
 
     def send_broadcast(self, socket):
-        broadcastPacket = NTPPacket()
-        broadcastPacket = NTPPacket(tx_timestamp = system_to_ntp_time(time.time()))
+        broadcastPacket = NTPPacket(tx_timestamp = time.time())
         broadcastPacket.poll = 10
         broadcastPacket.mode = 5
         broadcastPacket.ref_timestamp = broadcastPacket.tx_timestamp-5
+        self.pre_send_hook(broadcastPacket)
         logger.debug("Broadcast Packet details: \n%s", broadcastPacket)
         for bcast_address in self.broadcast_addresses:
             self.socket.sendto(broadcastPacket.to_data(),(bcast_address, self.port))
             logger.info("Sending broadcast packet to %s:%d", bcast_address,self.port)
 
-
-    def send_response(self, data, addr, recvTimestamp):
-        recvPacket = NTPPacket()
-        recvPacket.from_data(data)
+    def prepare_response(self, data, recvTimestamp):
+        recvPacket = NTPPacket.from_data(data)
         logger.debug("Received Packet details: \n%s", recvPacket)
-        timeStamp_high,timeStamp_low = recvPacket.GetTxTimeStamp()
         sendPacket = NTPPacket(version=3,mode=4)
         '''
         sendPacket.precision = 0xfa
         sendPacket.ref_id = 0x808a8c2c
         '''
+        sendPacket.orig_timestamp_high = recvPacket.tx_timestamp_high
+        sendPacket.orig_timestamp_low = recvPacket.tx_timestamp_low
         sendPacket.ref_timestamp = recvTimestamp-5
         sendPacket.recv_timestamp = recvTimestamp
-        sendPacket.tx_timestamp = system_to_ntp_time(time.time())
+        sendPacket.tx_timestamp = time.time()
+        self.pre_send_hook(sendPacket)
+        return sendPacket
+
+
+    def send_response(self, data, addr, recvTimestamp):
+        sendPacket = self.prepare_response(data, recvTimestamp)
         self.socket.sendto(sendPacket.to_data(),addr)
         logger.info("Sending response packet to %s:%d", addr[0],addr[1])
         logger.debug("Sent Packet details: \n%s", sendPacket)
 
+class WorkerThreadError(WorkerThread):
+    """Class which injects errors into the response"""
+
+    def __init__(self, *args, p_error, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.p_error = p_error
+
+    def originate_error(self, pkt):
+        logger.info("Injecting an error into the originate timestamp...")
+        pkt.orig_timestamp -= random.randint(1,1000)/10
+
+    def li_error(self, pkt):
+        logger.info("Setting LI to ALARM...")
+        pkt.leap = 3
+
+    def stratum_error(self, pkt):
+        new_stratum = random.choice((1,15,16))
+        logger.info("Setting stratum to: {}".format(new_stratum))
+        pkt.stratum = new_stratum
+
+    def vn_error(self, pkt):
+        new_version = pkt.version + random.choice((1,-1))
+        logger.info("Setting version to: {}".format(new_version))
+
+    def pre_send_hook(self, pkt):
+        if random.random() < self.p_error:
+            random.choice((
+                self.originate_error, self.li_error, self.stratum_error,
+                self.vn_error))(pkt)
 
 def get_parser():
     parser = argparse.ArgumentParser(description='SNTP server')
@@ -408,6 +462,8 @@ def get_parser():
             help='additionally log to a file')
     parser.add_argument('-b', type=int, default=0,
             help='broadcast_interval in secs, defaults to 0 (no broadcast)')
+    parser.add_argument('-e', type=float, default=0,
+            help='Probability of error injection, float 0-1, defaults to 0')
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -425,7 +481,10 @@ if __name__ == '__main__':
     socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     socket.bind((p.address,p.port))
     logger.info("local socket: %s", socket.getsockname());
-    worker_thread = workerThread(socket, p.b, p.port)
+    if p.e > 0:
+        worker_thread = WorkerThreadError(socket, p.b, p.port, p_error = p.e)
+    else:
+        worker_thread = WorkerThread(socket, p.b, p.port)
     worker_thread.start()
 
     while True:
